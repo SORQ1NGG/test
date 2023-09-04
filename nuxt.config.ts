094042cfef991d35980922e16f2ea17fa9e904fa
SORQ1NGG <daniil.sork@gmail.com>
2023-09-04 19:53:06 +0300

    chore: update configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     // @ts-ignore
-    css: ["~/assets/css/index.scss"],
+    css: ['~/assets/css/index.scss'],
+    modules: [
+        '@nuxtjs/eslint-module'
+    ]
 })
