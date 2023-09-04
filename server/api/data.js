5cd9158adab68b3f2877e8a2beb88007d21d5e7b
SORQ1NGG <daniil.sork@gmail.com>
2023-09-04 19:53:45 +0300

    chore: remove event param

--- server/api/data.js
+++ server/api/data.js
@@ -1,4 +1,4 @@
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async () => {
 
     return await $fetch(
         'https://6082e3545dbd2c001757abf5.mockapi.io/qtim-test-work/posts/')
