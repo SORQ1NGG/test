173584e1df847e975f9618525a685a09526b8865
SORQ1NGG <daniil.sork@gmail.com>
2023-09-10 03:14:19 +0300

    chore: added ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    typescript: {
+        strict: true,
+    },
     // @ts-ignore
     css: ['~/assets/css/index.scss'],
     modules: [
