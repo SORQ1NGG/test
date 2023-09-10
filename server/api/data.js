2a54d09895ae4ce93ea29a8a5e1b101478df429f
SORQ1NGG <daniil.sork@gmail.com>
2023-09-10 03:13:58 +0300

    chore: added eslint-disable-next-line no-undef

--- server/api/data.js
+++ server/api/data.js
@@ -1,5 +1,7 @@
+// eslint-disable-next-line no-undef
 export default defineEventHandler(async () => {
 
+    // eslint-disable-next-line no-undef
     return await $fetch(
         'https://6082e3545dbd2c001757abf5.mockapi.io/qtim-test-work/posts/')
 })
